2308242d587cde6e4e7b4f4d739f1f85d0fb3da6
JangKroed <didlsdydgh@gmail.com>
2022-10-03 18:11:19 +0900

    schema.indexl.js mongoDB 설정 변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,13 +1,28 @@
+const hidden = require("../config/dev");
 const mongoose = require("mongoose");
 
 const connect = () => {
+  if (process.env.Node_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+
   mongoose
-    .connect("mongodb://3.38.97.124:27017/AWS_spa_talk")
+    .connect(hidden.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "AWS_Spa_Talk",
+    })
+    .then(() => console.log("MongoDB connected..."))
     .catch((err) => console.log(err));
 };
 
-mongoose.connection.on("error", (err) => {
-  console.error("몽고디비 연결 에러", err);
+mongoose.connection.on("error", (error) => {
+  console.error("mongoDB connection error", error);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.error("Disconnected. try reconnect");
+  connect();
 });
 
 module.exports = connect;
