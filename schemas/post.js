bdd237b1b1336e93c4cb231d7a3018a86864be6a
JangKroed <didlsdydgh@gmail.com>
2022-10-03 15:38:16 +0900

    postId 스키마 제거, require 추가

--- schemas/post.js
+++ schemas/post.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 
 const postsSchema = new mongoose.Schema({
-  postId: {
-    type: String,
-  },
   user: {
     type: String,
+    require: true,
   },
   password: {
     type: String,
+    require: true,
   },
   title: {
     type: String,
+    require: true,
   },
   content: {
     type: String,
+    require: true,
   },
   createdAt: {
     type: Date,
